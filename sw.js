27f7bd7661e7e5d9cd6c67a98bd7d7fb8da3a6b5
sunfoxmai-cloud <sunfoxmai@gmail.com>
2025-10-19 11:16:04 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,26 @@
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open('liteshop-v1').then(c=>c.addAll(['./','./index.html','./styles.css','./app.js'])));
+const CACHE_NAME = 'liteshop-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
 });
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : undefined)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
